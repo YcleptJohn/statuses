ce608dbc011d44b83f389b721f47c1c7af3e26af
John Taylor <john.taylor@holidayextras.com>
2020-08-19 01:06:25 +0100

    Add uiMeta fields to config of gcloud

--- config/gcloud.js
+++ config/gcloud.js
@@ -1,7 +1,12 @@
 module.exports = {
   providerKey: 'gcloud',
   providerName: 'Google Cloud',
+  providerLogo: '//',
+  labels: [],
   hasRegions: false,
+  statusPageUrl: 'https://status.cloud.google.com/',
+  historicalStatusPageUrl: 'https://status.cloud.google.com/summary',
+  incidentDirectUriBase: 'https://status.cloud.google.com',
   fetchUrl: 'https://status.cloud.google.com/incidents.json',
   downDetectorIdentifier: 'google-cloud'
 }
