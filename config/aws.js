69d6b7f8e8f0b698ec31937f2b3973e1f581fe99
John Taylor <john.taylor@holidayextras.com>
2020-08-20 02:02:47 +0100

    Flesh out config for uiMeta

--- config/aws.js
+++ config/aws.js
@@ -1,7 +1,12 @@
 module.exports = {
   providerKey: 'aws',
   providerName: 'Amazon Web Services',
+  providerLogo: '//',
+  labels: [],
   hasRegions: true,
+  statusPageUrl: 'https://status.aws.amazon.com/',
+  historicalStatusPageUrl: 'https://aws.amazon.com/premiumsupport/technology/pes/',
+  incidentDirectUriBase: '',
   fetchUrl: 'https://status.aws.amazon.com/data.json',
   downDetectorIdentifier: 'aws-amazon-web-services'
 }
