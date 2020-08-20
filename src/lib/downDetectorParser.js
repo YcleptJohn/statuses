d08ad3c6a778b134e56e923b398f2fe2e8e98fe6
John Taylor <john.taylor@holidayextras.com>
2020-08-20 01:09:00 +0100

    Create a parser for converting downDetector HTML

---
+++ src/lib/downDetectorParser.js
@@ -0,0 +1,37 @@
+const ddParser = module.exports = {}
+const { parse } = require('node-html-parser')
+
+ddParser.jsonOverview = (ddHtml) => {
+  const fakeDom = parse(ddHtml, { script: true })
+  const reportVolume = ddParser._getReportVolume(fakeDom.querySelector('.entry-title').innerHTML)
+  const chartData = ddParser._extractChartData(fakeDom.querySelector('#chart-row').innerHTML)
+  return {
+    reportVolume,
+    chartData
+  }
+}
+
+ddParser._getReportVolume = (text) => {
+  if (text.toLowerCase().includes('no problems at')) return 'low'
+  if (text.toLowerCase().includes('possible problems at')) return 'medium'
+  if (text.toLowerCase().includes('problems at')) return 'high'
+}
+
+// Parse the <script> tags to extract the chart x,y report data - we can render our own graph in future
+ddParser._extractChartData = (chartHtml) => {
+  const chartDataRegex = /series:\s\[([\n\s{}a-zA-Z0-9'-:.+,]*)\]/
+  const matches = chartDataRegex.exec(chartHtml)
+  let chartData
+  if (matches && matches[1]) {
+    chartData = matches[1].split(',\n')
+      .map(x => { return x.replace(/\s/g, '') })
+      .filter(x => x)
+      .map(x => {
+        x = x.replace('x', '"x"')
+        x = x.replace('y', '"y"')
+        x = x.replace(/'/g, '"')
+        return JSON.parse(x)
+      })
+  }
+  return chartData
+}
