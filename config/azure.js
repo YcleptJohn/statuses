b250cf98cf113eedb0cfcf6234cc85b187804f18
John Taylor <john.taylor@holidayextras.com>
2020-08-21 21:39:36 +0100

    Flesh out azure config

--- config/azure.js
+++ config/azure.js
@@ -1,7 +1,12 @@
 module.exports = {
   providerKey: 'azure',
   providerName: 'Azure',
+  providerLogo: '//',
+  labels: [],
   hasRegions: true,
+  statusPageUrl: 'https://status.dev.azure.com/',
+  historicalStatusPageUrl: 'https://status.dev.azure.com/_history',
+  incidentDirectUriBase: '',
   fetchUrl: 'https://status.dev.azure.com/_rss',
   downDetectorIdentifier: 'windows-azure'
 }
