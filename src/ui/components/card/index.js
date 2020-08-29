e2d5004994ea9750f34fee65282cf97643516fbe
John Taylor <john.taylor@holidayextras.com>
2020-08-29 16:00:51 +0100

    Add downDetectorSummary to compact card view

--- src/ui/components/card/index.js
+++ src/ui/components/card/index.js
@@ -2,6 +2,7 @@ import { h } from 'preact';
 import style from './style.scss';
 import ModularCssHelper from '../../lib/ModularCssHelper.js';
 import IncidentsSummary from '../incidentsSummary';
+import DownDetectorSummary from '../downDetectorSummary';
 import statuses from '../../lib/statusConstants.js';
 
 const c = new ModularCssHelper(style)
@@ -29,6 +30,7 @@ const Card = ({ meta, status, data }) => {
             {status === statuses.IN_PROGRESS && <progress class={c.ss('progress is-small is-primary')} max='100' />}
             <IncidentsSummary data={data && data.ongoingIncidents} type='ongoing' />
             <IncidentsSummary data={data && data.recentIncidents} type='recent' />
+            <DownDetectorSummary data={data && data.downDetectorData} url={`https://downdetector.co.uk/status/${meta.downDetectorIdentifier}/`} />
           </div>
         </div>
         <footer class={c.ss('card-footer')}>
