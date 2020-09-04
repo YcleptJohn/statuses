04adf310fc60d9ad97133c5944777ba7bbe151b9
John Taylor <john.taylor@holidayextras.com>
2020-09-05 00:19:35 +0100

    Improve header styling and nav

--- src/ui/components/header/index.js
+++ src/ui/components/header/index.js
@@ -5,15 +5,19 @@ import ModularCssHelper from '../../lib/ModularCssHelper.js';
 const c = new ModularCssHelper(style)
 
 const Header = () => (
-	<nav class={c.ss('navbar level')} role="navigation" aria-label="main navigation">
-		<p class={c.ss('level-item has-text-centered is-unselectable is-size-4 has-text-weight-bold')}>
-			<span>TECH</span>
-			<span class={c.ss('icon is-medium has-text-danger')}>
-				<i class="fas fa-heartbeat fa" />
-				{/* <i class="fas fa-heart-broken fa" /> -- Logic to SWAP when a status is bad or USE STACK ABOVE? */}
-			</span>
-			<span>STATUS</span>
-		</p>
+	<nav class={c.ss('navbar')} role='navigation' aria-label='main navigation'>
+		<div class={c.ss('container centered-row')}>
+			<div class={c.ss('navbar-brand centered-row')}>
+				<a class={c.ss('navbar-item is-size-4 consistent-colour')} href={'/'}>
+					<span class={c.ss('icon is-medium has-text-grey-dark')}>
+						<i class={c.ss('fas fa-layer-group')} />
+					</span>
+					<span>statuses</span>
+					<span>.</span>
+					<span class={c.ss('has-text-info')}>tech</span>
+				</a>
+			</div>
+		</div>
 	</nav>
 );
 
