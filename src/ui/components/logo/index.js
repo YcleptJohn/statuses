d7ffe68e2caf6d3ddbca42df5e7a9ab74f70da7d
John Taylor <john.taylor@holidayextras.com>
2020-09-06 04:28:18 +0100

    Add inline variant to logo

--- src/ui/components/logo/index.js
+++ src/ui/components/logo/index.js
@@ -1,4 +1,4 @@
-import { h, Fragment } from 'preact'
+import { h } from 'preact'
 import style from './style.scss'
 import ModularCssHelper from '../../lib/ModularCssHelper.js'
 import { FontAwesomeIcon } from '@aduh95/preact-fontawesome'
@@ -6,14 +6,14 @@ import { faLayerGroup } from '@fortawesome/free-solid-svg-icons'
 
 const c = new ModularCssHelper(style)
 
-const Logo = ({ small }) => (
-  <div class={c.ss('logo-font logo-wrapper')}>
+const Logo = ({ small, inline }) => (
+  <div class={c.ss(`logo-font logo-wrapper is-unselectable ${inline ? 'is-inline-flex' : ''}`)}>
     <span class={c.ss(`icon ${small ? 'is-small mr-1' : 'is-medium'} has-text-grey-dark`)}>
       <FontAwesomeIcon icon={faLayerGroup} />
     </span>
     <span>statuses</span>
     <span>.</span>
-    <span class={c.ss('has-text-info')}>tech</span>
+    <span class={c.ss('has-text-primary')}>tech</span>
   </div>
 )
 
