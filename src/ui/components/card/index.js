75ef7fba42d174776ca74b0cbc9b9019b241d94c
John Taylor <john.taylor@holidayextras.com>
2020-09-10 01:22:41 +0100

    Tidyup loading style on homepage cards, fix size diff

--- src/ui/components/card/index.js
+++ src/ui/components/card/index.js
@@ -4,8 +4,6 @@ import ModularCssHelper from '../../lib/ModularCssHelper.js';
 import IncidentsSummary from '../incidentsSummary';
 import DownDetectorSummary from '../downDetectorSummary';
 import statuses from '../../lib/statusConstants.js';
-import { FontAwesomeIcon } from '@aduh95/preact-fontawesome'
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 
 const c = new ModularCssHelper(style)
 
@@ -21,7 +19,6 @@ const Card = ({ meta, status, data }) => {
         <header class={c.ss(`card-header ${headerColour}`)}>
           <p class={c.ss('card-header-title is-centered')}>
             {meta.providerName}
-            {status === statuses.IN_PROGRESS && <FontAwesomeIcon icon={faCircleNotch} spin fixedWidth className={c.ss('has-text-primary ml-2')} />}
           </p>
         </header>
         <div class={c.ss('card-content')}>
@@ -29,14 +26,26 @@ const Card = ({ meta, status, data }) => {
             <img src={`/assets/${meta.providerLogo}`} width='256' height='256' class={c.ss('scaled-logo')} />
           </div>
           <div class={c.ss('content')}>
-            {status === statuses.IN_PROGRESS && <progress class={c.ss('progress is-small is-primary')} max='100' />}
-            <IncidentsSummary data={data && data.ongoingIncidents} type='ongoing' />
-            <IncidentsSummary data={data && data.recentIncidents} type='recent' />
-            <DownDetectorSummary data={data && data.downDetectorData} url={`https://downdetector.co.uk/status/${meta.downDetectorIdentifier}/`} />
+            {status === statuses.IN_PROGRESS
+            ? (
+              <>
+                <progress class={c.ss('progress is-small is-primary')} max='100' />
+                <div class={c.ss('loading-text')}>
+                  <img src={`/assets/logo-loading.gif`} class={c.ss('loading-logo')} />
+                  <h5 class={c.ss('title is-size-5 has-text-grey-dark is-italic')}>Checking status...</h5>
+                </div>
+              </>
+            ) : (
+              <>
+                <IncidentsSummary data={data && data.ongoingIncidents} type='ongoing' />
+                <IncidentsSummary data={data && data.recentIncidents} type='recent' />
+                <DownDetectorSummary data={data && data.downDetectorData} url={`https://downdetector.co.uk/status/${meta.downDetectorIdentifier}/`} />
+              </>
+            )}
           </div>
         </div>
         <footer class={c.ss('card-footer')}>
-          <a href={`/service/${meta.providerKey}`} class={c.ss('card-footer-item')}>See More</a>
+          {status === statuses.COMPLETED_SUCCESSFULLY && <a href={`/service/${meta.providerKey}`} class={c.ss('card-footer-item')}>See More</a>}
         </footer>
       </div>
     </div>
