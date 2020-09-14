a5f1b9bdaf50b41461c12695acd138f081602eac
John Taylor <john.taylor@holidayextras.com>
2020-09-14 23:43:50 +0100

    Termporarily disable CSP until we can script nonces

--- src/api/server.js
+++ src/api/server.js
@@ -8,20 +8,7 @@ const router = express()
 
 router.use(
   helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        baseUri: ["'self'"],
-        blockAllMixedContent: [],
-        fontSrc: ["'self'", 'https:', 'data:'],
-        frameAncestors: ["'self'"],
-        imgSrc: ["'self'", 'data:'],
-        objectSrc: ["'none'"],
-        scriptSrc: ["'self'"],
-        styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
-        upgradeInsecureRequests: []
-      }
-    }
+    contentSecurityPolicy: false
   })
 )
 router.use(cors())
