b516774fab4d8780eeb25bdd97d3bdbea505fe2a
John Taylor <john.taylor@holidayextras.com>
2020-09-14 22:51:17 +0100

    Explicitly enable csp-helmet

--- src/api/server.js
+++ src/api/server.js
@@ -11,7 +11,21 @@ router.use(express.static(path.resolve(__dirname, '..', '..', 'build')))
 router.use(express.json())
 router.use(
   helmet({
-    contentSecurityPolicy: false
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        baseUri: ["'self'"],
+        blockAllMixedContent: [],
+        fontSrc: ["'self'", 'https:', 'data:'],
+        frameAncestors: ["'self'"],
+        imgSrc: ["'self'", 'data:'],
+        objectSrc: ["'none'"],
+        scriptSrc: ["'self'"],
+        scriptSrcAttr: ["'self'"],
+        styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
+        upgradeInsecureRequests: []
+      }
+    }
   })
 )
 router.use(cors())
